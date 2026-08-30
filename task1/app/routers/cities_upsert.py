f6db197f419f3b65c5cb17aff7a7ffa2fbccb0d1
Mehrshad Fallah <115925170+Mhrshd-fllh@users.noreply.github.com>
2026-08-30 16:39:25 +0330

    Fix/task1 post api exception handler (#14)
    * fix: fixed status code for creating and updating, fixed race condition, fixed error logger
    
    * fix: fixed error

--- task1/app/routers/cities_upsert.py
+++ task1/app/routers/cities_upsert.py
@@ -2,20 +2,24 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy import func, select, text
+from sqlalchemy.exc import SQLAlchemyError
 
+import logging
 from app.cache.redis_manager import RedisManager, get_redis_manager
 from app.database import get_db_session
 from app.models import CityCountry
 from app.schemas import CityUpsertRequest, CityResponse
 
+logger = logging.getLogger("Cities_upsert")
+
 router = APIRouter(prefix="/cities", tags=["Cities"])
 
-@router.post("", status_code=status.HTTP_201_CREATED, 
+@router.post("", status_code=status.HTTP_200_OK, 
              summary="Create or update a City-Country record", 
              response_model=CityResponse, 
              description="This endpoint allows you to insert a new city-country record or update an existing one based on the city name. If the city already exists, its country code will be updated.")
 async def upsert_city(payload: CityUpsertRequest, db: AsyncSession = Depends(get_db_session), 
-                      cache_mangager: RedisManager = Depends(get_redis_manager)) -> CityResponse:
+                      cache_manager: RedisManager = Depends(get_redis_manager)) -> CityResponse:
     """
     Upsert a city-country record in the database.
 
@@ -25,20 +29,20 @@ async def upsert_city(payload: CityUpsertRequest, db: AsyncSession = Depends(get
     If the city already exists, its country code will be updated. If it doesn't exist, a new record will be created.
     """
     try:
-        existing_stmt = select(CityCountry).where(func.lower(CityCountry.city_name) == func.lower(payload.city))
-        existing_result = await db.execute(existing_stmt)
-        existing_record = existing_result.scalar_one_or_none()
-        is_created = existing_record is None
-
         upper_stmt = (
-            insert(CityCountry).values(
+            insert(CityCountry)
+            .values(
                 city_name=payload.city,
                 country_code=payload.country_code
-            ).on_conflict_do_update(
-                constraint="ix_city_country_city_name_lower",
-                index_elements = [text("LOWER(city_name)")],
-                set_={"country_code": payload.country_code, "updated_at": func.now()}
-            ).returning(CityCountry)
+            )
+            .on_conflict_do_update(
+                index_elements=[text("LOWER(city_name)")],
+                set_={
+                    "country_code": payload.country_code,
+                    "updated_at": func.now()
+                }
+            )
+            .returning(CityCountry)
         )
 
         result = await db.execute(upper_stmt)
@@ -46,7 +50,7 @@ async def upsert_city(payload: CityUpsertRequest, db: AsyncSession = Depends(get
 
         await db.commit()
 
-        await cache_mangager.invalidate_city(payload.city)
+        await cache_manager.invalidate_city(payload.city)
 
         return CityResponse(
             id=upserted_record.id,
@@ -54,10 +58,11 @@ async def upsert_city(payload: CityUpsertRequest, db: AsyncSession = Depends(get
             country_code=upserted_record.country_code,
             created_at=upserted_record.created_at,
             updated_at=upserted_record.updated_at,
-            message="created" if is_created else "updated"
+            message="created" if upserted_record.created_at == upserted_record.updated_at else "updated"
         )
             
 
-    except:
+    except SQLAlchemyError as err:
         await db.rollback()
+        logger.error("Error raised '%s'", err)
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Database query failed.")
